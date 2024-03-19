a2483f6ad1b463475039a7a5a31b9d74797d87f0
Joseph-exe <josepraali@hotmail.com>
2024-03-19 09:32:27 -0300

    mapa interactivo sin enrutamiento a los modulos

---
+++ src/app/grupo3/components/mapa/tab1.page.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+  map!: Map;
+
+  ngAfterViewInit(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+}
