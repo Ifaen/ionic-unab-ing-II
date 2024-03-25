c5875c67bb59574d00822137281dd3632fd94453
Elimaster36 <eliasmarzal188@gmail.com>
2024-03-25 16:34:09 -0300

    Avance Grupo 2
    Registro de Usuario
    Perfil de Usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RegisterPage } from './register/register.page';
+
 
 const routes: Routes = [
   {
@@ -8,8 +10,16 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'register',
     pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then( m => m.UsuarioPageModule)
   },
 ];
 
