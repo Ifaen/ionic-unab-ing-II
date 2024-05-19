e8d93a62a5612104df9f5db83eed608b2c4311cc
Migueliedo1610 <162817805+Migueliedo1610@users.noreply.github.com>
2024-05-19 17:53:34 -0400

    sombra a los filtros
    le puse a los botones de los filtros una sombra alrededor del boton para que el usuario distinga que filtros estan seleccionados

--- src/app/shared/component/filtros/filtros.component.ts
+++ src/app/shared/component/filtros/filtros.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { collectionGroup } from "firebase/firestore";
-import { Feature } from "ol";
-import { Point } from "ol/geom";
-import { Icon, Style } from "ol/style";
-import { Report, ReportIcon } from "src/app/models/report.model";
+import { Style, Icon } from "ol/style";
+import { ReportIcon } from "src/app/models/report.model";
 import { ReportService } from "src/app/services/report.service";
 
 @Component({
@@ -12,7 +9,7 @@ import { ReportService } from "src/app/services/report.service";
   styleUrls: ["./filtros.component.scss"],
 })
 export class FiltrosComponent implements OnInit {
-  private filters: { [key: string]: boolean } = {
+  public filters: { [key: string]: boolean } = {
     "accidente-vehicular": true,
     alumbrado: true,
     basura: true,
@@ -26,24 +23,14 @@ export class FiltrosComponent implements OnInit {
   ngOnInit() {}
 
   public filterReports(module: string): void {
-    for (const key in this.filters) {
-      if (key === module) {
-        this.filters[key] = !this.filters[key]; // Invertir valor actual
-        break;
-      }
-    }
+    this.filters[module] = !this.filters[module]; // Invertir valor actual
 
     this.reportIcons.forEach((report) => {
-      if (report.data.module == module) {
+      if (report.data.module === module) {
         const src: string = this.reportService.getIcon(module); // Obtener el icon del reporte segun su modulo
 
         // Definir el tamaño del icono
-        let scale: number;
-        if (this.filters[module]) {
-          scale = 0.08;
-        } else {
-          scale = 0; // Ocultar icono
-        }
+        const scale = this.filters[module] ? 0.08 : 0; // Mostrar u ocultar icono
 
         report.iconFeature.setStyle(
           new Style({
