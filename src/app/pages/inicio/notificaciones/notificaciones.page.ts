0371677478e84f131558b91a35036d5772c2d6b1
maarko <maarko.burgos@gmail.com>
2024-06-08 18:16:23 -0400

    Botón de Notificaciones
    - Apartado de Notificaciones Hecho yico
    
    - Falta hacerlo bonito chicos owo
    
    - Ya esta conectado a la base de datos uwu yico owo PatoDonald

--- src/app/pages/inicio/notificaciones/notificaciones.page.ts
+++ src/app/pages/inicio/notificaciones/notificaciones.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from 'src/app/services/report.service';
+import { Report, ReportIncendio, ReportAlumbrado, ReportBasura, ReportVehicular } from 'src/app/models/report.model';
 
 @Component({
   selector: 'app-notificaciones',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificaciones.page.scss'],
 })
 export class NotificacionesPage implements OnInit {
+  public reports: (Report | ReportIncendio | ReportAlumbrado | ReportVehicular | ReportBasura)[] = [];
 
-  constructor() { }
+  constructor(public reportService: ReportService) { }
 
   ngOnInit() {
+    this.loadReports();
   }
 
+  // Ordena las Fechas
+  async loadReports() {
+    this.reports = await this.reportService.getReports();
+    this.reports.sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      return dateB - dateA; // Ordenar de más reciente a más antiguo
+    });
+  }
+
+  public getIcon(module: string): string {
+    return this.reportService.getIcon(module);
+  }
+
+  public hasTypeIncident(report: Report | ReportIncendio | ReportAlumbrado | ReportVehicular | ReportBasura): report is ReportIncendio | ReportAlumbrado | ReportVehicular | ReportBasura {
+    return 'typeIncident' in report;
+  }
 }
